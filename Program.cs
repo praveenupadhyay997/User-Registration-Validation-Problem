951eec4bdd05c31829597a3079b3be2e843a44c0
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-05 16:55:05 +0530

    [Praveen] Add: Welcome message to the User Registration Validation Problem

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UserRegistrationValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("================================================");
+            Console.WriteLine("Welcome to User Registration Validation Program");
+            Console.WriteLine("================================================");
+        }
+    }
+}
